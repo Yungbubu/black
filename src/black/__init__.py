c1a44d4d33893421bb3205e01efcad3f954e0be9
YungBubu <yann.bury@gmail.com>
2020-10-12 16:48:06 +0200

    Use 2-spaces indents

--- src/black/__init__.py
+++ src/black/__init__.py
@@ -1738,7 +1738,7 @@ class Line:
         if not self:
             return "\n"
 
-        indent = "    " * self.depth
+        indent = "  " * self.depth
         leaves = iter(self.leaves)
         first = next(leaves)
         res = f"{first.prefix}{indent}{first.value}"
